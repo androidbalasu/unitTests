c1908cb31bad2d6e847674a087ed2560688768d7
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-08-16 14:32:53 -0500

    Refactor absolution function and rerun the test to make sure it passes.

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,17 +1,22 @@
 const lib = require('../lib');
 
-test ('Absolute - should return positive number if input is positive', () => {
-    const result = lib.absolute(1);
-    expect(result).toBe(1);
+//Use describe to group a bunch of related tests.
+describe('absolute', ()=>{
+    it ('should return positive number if input is positive', () => {
+        const result = lib.absolute(1);
+        expect(result).toBe(1);
+    });
+    
+    it ('should return positive number if input is negative', () => {
+        const result = lib.absolute(-1);
+        expect(result).toBe(1);
+    });
+    
+    it ('should return zero if input is zero', () => {
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
 });
 
-test ('Absolute - should return positive number if input is negative', () => {
-    const result = lib.absolute(-1);
-    expect(result).toBe(1);
-});
 
-test ('Absolute - should return zero if input is zero', () => {
-    const result = lib.absolute(0);
-    expect(result).toBe(0);
-});
 
