79143a8d0a9e49e47ee1b166acdacd4c162da31f
Prashanth.Anantharam <Prashanth.Anantharam@bentley.com>
2018-08-16 14:18:00 -0500

    Add tests for each code path in the absolution function in the lib module.

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,3 +1,17 @@
-test ('Our first test', ()=>{
- throw new Error('Somethign failed');
+const lib = require('../lib');
+
+test ('Absolute - should return positive number if input is positive', () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
 });
+
+test ('Absolute - should return positive number if input is negative', () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+});
+
+test ('Absolute - should return zero if input is zero', () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+});
+
